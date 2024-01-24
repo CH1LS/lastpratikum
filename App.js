4f88e0e4c975379ebb79ed21662872aa018049cd
CH1LS <144717405+CH1LS@users.noreply.github.com>
2024-01-24 15:06:51 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./boxmodel/navbar.css"
+const Navbar = () =>{
+    return (
+        <nav>
+            <img src="https://thumbs.dreamstime.com/b/abstract-wings-logo-design-vector-template-isolated-white-background-abstract-wings-logo-design-vector-template-194545808.jpg" />
+            <ul>
+                <li>
+                     <a>home</a>
+                </li>
+                <li>
+                     <a>about</a>
+                </li>
+                <li>
+                     <a>content</a>
+                </li>
+                <li>
+                     <a>home</a>
+                </li>
+            
+            </ul>
+        </nav>
+    
+    
+    )
+}
+       
+
+
+export default Navbar;
+
---
+++ header.js
@@ -0,0 +1,3 @@
+import React from "react";
+import header from "./boxmodel/header.css"
+import icon from "./boxmodel/isi.css"
